f857ebf85d329e7a4753a43683a095649a9b4ba0
Auriane Gabaut <122619227+aurianegbt@users.noreply.github.com>
2024-02-23 11:16:39 +0100

    Clean and remake simulation

--- Graphs.R
+++ Graphs.R
@@ -5,74 +5,89 @@ library(scales)
 library(dplyr)
 # Custom Theme
 source("~/Travail/00_Theme.R")
+
+# Args
+data = c("AB","G1","G2")
+files ="Mult"
+Nbr_ind = 15
+
 # Load data
-DataSet = read.csv("IreneModel2/Simulation/simulatedData_Ab_G1.csv") %>%
-  filter(obsid=="yAB")
+sim.list = lapply(data,FUN=function(d){read.csv(paste0("Model/IreneModel",files,"/Simulation/simulation_y",d,".txt"))})
 
-moy = aggregate(DataSet[,"obs"],by = list(DataSet[,"TIME"]),FUN=function(x){mean(x,na.rm=TRUE)})
+Dataset <- data.frame()
+for(d in 1:length(data)){
+  Dataset <- Dataset %>% rbind(sim.list[[d]] %>% mutate(obsid = paste0("y",data[d]),.before = group) %>% rename(obs = paste0("y",data[d]))
+  )
+}
 
-med = aggregate(DataSet[,"obs"],by = list(DataSet[,"TIME"]),FUN=function(x){median(x,na.rm=TRUE)})
+Dataset <- Dataset %>%
+  filter(rep <= 5) %>%
+  filter(if(Nbr_ind==50){group=="simulationGroup1"}else if(Nbr_ind==15){group=="simulationGroup2"}) %>%
+  mutate(id=if(Nbr_ind==15){id-50}else{id}) %>%
+  mutate(rep_id=paste0(rep,"_",id),.before = "id") %>%
+  select(-group,-id,-rep)
 
-sd = merge(aggregate(DataSet[,"obs",drop=FALSE],by = list(DataSet[,"TIME"]),FUN=function(x){sd(x,na.rm=TRUE)}), moy,by="Group.1")
+if("AB" %in% data){
+  DataSet = Dataset %>% filter(obsid=="yAB")
+  moy = aggregate(DataSet[,"obs"],by = list(DataSet[,"time"]),FUN=function(x){mean(x,na.rm=TRUE)})
 
-sd = as.data.frame(cbind(TIME=sd$Group.1,lcb = sd$x-1.96*sd$obs, ucb = sd$x +1.96*sd$obs,mean=sd$x))
+  med = aggregate(DataSet[,"obs"],by = list(DataSet[,"time"]),FUN=function(x){median(x,na.rm=TRUE)})
 
-color = colorRampPalette(cbPalette)(length(unique(DataSet[DataSet$rep==1,"ID"])))
+  sd = merge(aggregate(DataSet[,"obs",drop=FALSE],by = list(DataSet[,"time"]),FUN=function(x){sd(x,na.rm=TRUE)}), moy,by="Group.1")
+  sd = as.data.frame(cbind(TIME=sd$Group.1,lcb = sd$x-1.96*sd$obs, ucb = sd$x +1.96*sd$obs,mean=sd$x))
 
-ggplot()+
-  geom_point(data=DataSet[DataSet$rep==1,],mapping=aes(x=TIME,y=obs,group=UID,color=UID))+
-  geom_line(data=DataSet[DataSet$rep==1,],mapping=aes(x=TIME,y=obs,group=UID,color=UID),alpha=0.4)+
-  geom_line(data=sd,mapping=aes(x=TIME,y=mean),lwd=1)+
-  # geom_errorbar(data=sd,aes(x=TIME,ymin=lcb, ymax=ucb),lwd=0.8)+
-  xlab("Time (in days)")+
-  ylab("Antibody concentrations")+
-  theme(axis.text = element_text(size=8))+
-  theme(axis.title = element_text(size=14))+
-  theme(plot.title = element_blank())+
-  theme(plot.subtitle = element_text(hjust=0.5))+
-  theme(strip.text = element_text(size = 16))+
-  scale_color_manual(values=color)+
-  theme(legend.position = "none")
-########################################################################
-DataSet = read.csv("IreneModel2/Simulation/simulatedData_Ab_G1.csv") %>%
-  filter(obsid=="yG1")
+  color = colorRampPalette(cbPalette)(length(unique(DataSet[,"rep_id"])))
 
-moy = aggregate(DataSet[,"obs"],by = list(DataSet[,"TIME"]),FUN=function(x){mean(x,na.rm=TRUE)})
+  ggplot()+
+    geom_point(data=DataSet,mapping=aes(x=time,y=obs,group=rep_id,color=rep_id))+
+    geom_line(data=DataSet,mapping=aes(x=time,y=obs,group=rep_id,color=rep_id),alpha=0.4)+
+    geom_line(data=sd,mapping=aes(x=TIME,y=mean),lwd=1)+
+    geom_errorbar(data=sd,aes(x=TIME,ymin=lcb, ymax=ucb),lwd=0.8)+
+    xlab("Time (in days)")+
+    ylab("Antibody concentrations")+
+    theme(axis.text = element_text(size=8))+
+    theme(axis.title = element_text(size=14))+
+    theme(plot.title = element_blank())+
+    theme(plot.subtitle = element_text(hjust=0.5))+
+    theme(strip.text = element_text(size = 16))+
+    scale_color_manual(values=color)+
+    theme(legend.position = "none")
 
-med = aggregate(DataSet[,"obs"],by = list(DataSet[,"TIME"]),FUN=function(x){median(x,na.rm=TRUE)})
+  if(!dir.exists(paste0("~/Travail/Presentation/Plot/PhD_reflective_Irene/",files))){dir.create(paste0("~/Travail/Presentation/Plot/PhD_reflective_Irene/",files))}
+  ggsave(filename = paste0("~/Travail/Presentation/Plot/PhD_reflective_Irene/",files,"/Ab",Nbr_ind,"_errorBar.png"))
+}
 
-sd = merge(aggregate(DataSet[,"obs",drop=FALSE],by = list(DataSet[,"TIME"]),FUN=function(x){sd(x,na.rm=TRUE)}), moy,by="Group.1")
+for(d in data[stringr::str_detect(data,"G")]){
+  DataSet <- Dataset %>% filter(obsid==paste0("y",d))
 
-sd = as.data.frame(cbind(TIME=sd$Group.1,lcb = sd$x-1.96*sd$obs, ucb = sd$x +1.96*sd$obs,mean=sd$x))
+  moy = aggregate(DataSet[,"obs"],by = list(DataSet[,"time"]),FUN=function(x){mean(x,na.rm=TRUE)})
 
-color = colorRampPalette(cbPalette)(length(unique(DataSet[DataSet$rep==1,"ID"])))
+  med = aggregate(DataSet[,"obs"],by = list(DataSet[,"time"]),FUN=function(x){median(x,na.rm=TRUE)})
 
-ggplot()+
-  geom_point(data=DataSet[DataSet$rep==1,],mapping=aes(x=TIME,y=obs,group=UID,color=UID),alpha=0.3)+
-  geom_line(data=DataSet[DataSet$rep==1,],mapping=aes(x=TIME,y=obs,group=UID,color=UID),alpha=0.4)+
-  geom_line(data=sd,mapping=aes(x=TIME,y=mean),lwd=1)+
-  geom_errorbar(data=sd,aes(x=TIME,ymin=lcb, ymax=ucb),lwd=0.8)+
-  xlab("Time (in days)")+
-  ylab("Antibody concentrations")+
-  theme(axis.text = element_text(size=8))+
-  theme(axis.title = element_text(size=14))+
-  theme(plot.title = element_blank())+
-  theme(plot.subtitle = element_text(hjust=0.5))+
-  theme(strip.text = element_text(size = 16))+
-  scale_color_manual(values=color)+
-  theme(legend.position = "none")
+  sd = merge(aggregate(DataSet[,"obs",drop=FALSE],by = list(DataSet[,"time"]),FUN=function(x){sd(x,na.rm=TRUE)}), moy,by="Group.1")
 
-color = colorRampPalette(cbPalette)(5)
+  sd = as.data.frame(cbind(TIME=sd$Group.1,lcb = sd$x-1.96*sd$obs, ucb = sd$x +1.96*sd$obs,mean=sd$x))
 
-ggplot()+
-  geom_point(data=DataSet[DataSet$rep==1 & DataSet$ID <=5,],mapping=aes(x=TIME,y=obs,group=UID,color=UID),alpha=0.3)+
-  geom_line(data=DataSet[DataSet$rep==1 & DataSet$ID <=5,],mapping=aes(x=TIME,y=obs,group=UID,color=UID))+
-  xlab("Time (in days)")+
-  ylab("Antibody concentrations")+
-  theme(axis.text = element_text(size=8))+
-  theme(axis.title = element_text(size=14))+
-  theme(plot.title = element_blank())+
-  theme(plot.subtitle = element_text(hjust=0.5))+
-  theme(strip.text = element_text(size = 16))+
-  scale_color_manual(values=color)+
-  theme(legend.position = "none")
+
+  color = colorRampPalette(cbPalette)(length(unique(DataSet[,"rep_id"])))
+
+
+  ggplot()+
+    geom_point(data=DataSet,mapping=aes(x=time,y=obs,group=rep_id,color=rep_id),alpha=0.3)+
+    geom_line(data=DataSet,mapping=aes(x=time,y=obs,group=rep_id,color=rep_id),alpha=0.4)+
+    geom_line(data=sd,mapping=aes(x=TIME,y=mean),lwd=1)+
+    geom_errorbar(data=sd,aes(x=TIME,ymin=lcb, ymax=ucb),lwd=0.8)+
+    xlab("Time (in days)")+
+    ylab(paste0("Genes ",d," expression"))+
+    theme(axis.text = element_text(size=8))+
+    theme(axis.title = element_text(size=14))+
+    theme(plot.title = element_blank())+
+    theme(plot.subtitle = element_text(hjust=0.5))+
+    theme(strip.text = element_text(size = 16))+
+    scale_color_manual(values=color)+
+    theme(legend.position = "none")
+  ggsave(filename = paste0("~/Travail/Presentation/Plot/PhD_reflective_Irene/",files,"/",d,Nbr_ind,"_errorBar.png"))
+}
+
+
+
