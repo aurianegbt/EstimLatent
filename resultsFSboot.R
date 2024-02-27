f2bdc8b6ceba94a1ca4205941eac2c9356bf151c
Auriane Gabaut <122619227+aurianegbt@users.noreply.github.com>
2024-02-27 10:36:45 +0100

    Modify reasult display

--- resultsFSboot.R
+++ resultsFSboot.R
@@ -1,14 +1,16 @@
 library(dplyr)
 library(flextable)
 ## DATA INFORMATION
-data = "Ab"
-type="only_S"
+data = c("AB","G2")
+type="default"
 files="Mult"
+Nind = "15"
 
-load(paste0("Results/Results",files,"_",data,"/bootstrap",type,"/estim_1.RData"))
-nBoot = length(resBoot)
+load(paste0("Results/Results",files,if(Nind!=50){Nind},"_",paste0(data,collapse="_"),"/bootstrap",type,"/estim_1.RData"))
+# nBoot = length(resBoot)
+nBoot = 250
 
-# nrep = length(list.files(paste0("Results/Results",files,"_",data,"/bootstrap",type)))
+# nrep = length(list.files(paste0("Results/Results",files,"_",paste0(data,collapse="_"),"/bootstrap",type)))
 nrep = 100
 precision = abs(floor(log10(1/nrep)))
 
@@ -20,7 +22,17 @@ EstimatedPopulationParameters = data.frame() # tableau des paramètres estimés 
 EstimatedStandardDeviationBOOT = data.frame() # tableau des écart-type estimées par bootstrap (sd des paramètres estimés sur chaque boot du réplicat)
 EstimatedPopulationParametersBOOT = data.frame() # tableau du paramètre estimées par bootstrap (mean des paramètres estimées sur chaque boot du réplicat)
 
-load(paste0("Results/Results",files,"_",data,"/",type,"/PopulationParameters_theo.RData"))
+load(paste0("Results/PopulationParameters_theo_",files,".RData"))
+if(!("G1" %in% data)){
+  PopulationParameters <- PopulationParameters %>% select(-alpha_1_pop,-sigma_G1)
+}
+if(!("G2" %in% data)){
+  PopulationParameters <- PopulationParameters %>% select(-alpha_2_pop,-sigma_G2)
+}
+if(type=="only_S"){
+  PopulationParameters <- PopulationParameters %>% select(-delta_Ab_pop)
+}
+
 
 for(f in 1:nrep){
   if(exists("resBoot"))
@@ -28,15 +40,15 @@ for(f in 1:nrep){
   if(exists("res"))
     rm(res)
   suppressWarnings(tryCatch({
-    pathToRes = paste0("Results/Results",files,"_",data,"/bootstrap",type,"/estim_",f,".RData")
+    pathToRes = paste0("Results/Results",files,if(Nind!=50){Nind},"_",paste0(data,collapse="_"),"/bootstrap",type,"/estim_",f,".RData")
     load(pathToRes)
-    if(length(resBoot)!=500)
+    if(length(resBoot)<nBoot)
       cat("file n°",f," : ",length(resBoot),"\n")
 
       EstimatedStandardDeviation = rbind(EstimatedStandardDeviation, t(res[,"StandardError",drop=FALSE]))
       EstimatedPopulationParameters = rbind(EstimatedPopulationParameters,t(res[,"Parameters",drop=FALSE]))
 
-    Est = Reduce(cbind,lapply(resBoot,function(dt){dt[,"Parameters",drop=FALSE]}))
+    Est = Reduce(cbind,lapply(resBoot[1:nBoot],function(dt){dt[,"Parameters",drop=FALSE]}))
 
     EstimatedStandardDeviationBOOT = rbind(EstimatedStandardDeviationBOOT, t(as.data.frame(apply(Est,1,sd))))
     EstimatedPopulationParametersBOOT = rbind(EstimatedPopulationParametersBOOT,t(as.data.frame(rowMeans(Est))))
@@ -49,7 +61,16 @@ for(f in 1:nrep){
 
   },error=function(e){cat(f,",")}))
 }
-
+if("sigma_Ab" %in% colnames(EstimatedStandardDeviation)){
+  EstimatedStandardDeviation <- EstimatedStandardDeviation %>%
+    rename(sigma_AB=sigma_Ab)
+  EstimatedStandardDeviationBOOT <- EstimatedStandardDeviationBOOT %>%
+    rename(sigma_AB=sigma_Ab)
+  EstimatedPopulationParameters <- EstimatedPopulationParameters%>%
+    rename(sigma_AB=sigma_Ab)
+  EstimatedPopulationParametersBOOT <- EstimatedPopulationParametersBOOT %>%
+    rename(sigma_AB=sigma_Ab)
+}
 EstimatedStandardDeviation <- t(EstimatedStandardDeviation)[names(PopulationParameters),]
 EstimatedPopulationParameters <- t(EstimatedPopulationParameters)[names(PopulationParameters),]
 
@@ -64,8 +85,8 @@ stats_estim = data.frame(target_values = setNames(as.numeric(PopulationParameter
 stats_estim <- cbind(stats_estim,
                      mean=rowMeans(EstimatedPopulationParameters),
                      meanBoot = rowMeans(EstimatedPopulationParametersBOOT),
-                     bias=as.numeric(((rowMeans(EstimatedPopulationParameters)-stats_estim[,'target_values']))/(PopulationParameters)),
-                     biasBoot = as.numeric(((rowMeans(EstimatedPopulationParametersBOOT)-stats_estim[,'target_values']))/(PopulationParameters)),
+                     bias=as.numeric(((rowMeans(EstimatedPopulationParameters)-stats_estim[,'target_values']))/as.numeric((PopulationParameters))),
+                     biasBoot = as.numeric(((rowMeans(EstimatedPopulationParametersBOOT)-stats_estim[,'target_values']))/as.numeric((PopulationParameters))),
                      sd_emp = apply(EstimatedPopulationParameters,1,sd),
                      sd_hess = rowMeans(apply(EstimatedStandardDeviation,MARGIN=2,FUN=as.numeric),na.rm = TRUE),
                      sd_boot = rowMeans(apply(EstimatedStandardDeviationBOOT,MARGIN=2,FUN=as.numeric),na.rm = TRUE))
@@ -122,306 +143,39 @@ if("countNA" %in% colnames(stats_estim)){
 }
 
 
-if(files==""){
-  if(data=="Ab"){
-    tex2_names = c(
-     "{f_M}_1",
-     "\\delta_S",
-     "\\theta",
-     "\\delta_{Ab}",
-     "\\omega_{{f_M}_1}",
-     "\\omega_{\\delta_S}",
-     "\\omega_\\theta",
-     "\\sigma_{AB}"
-    )
-  }else if(data=="Ab_G1"){
-    tex2_names = c(
-     "{f_M}_1",
-     "\\delta_S",
-     "\\theta",
-     "\\delta_{Ab}",
-     "\\alpha_1",
-     "\\omega_{{f_M}_1}",
-     "\\omega_{\\delta_S}",
-     "\\omega_\\theta",
-     "\\sigma_{AB}",
-     "\\sigma_{G1}"
-    )
-  }else if(data=="Ab_G2"){
-    tex2_names = c(
-     "{f_M}_1",
-     "\\delta_S",
-     "\\theta",
-     "\\delta_{Ab}",
-     "\\alpha_2",
-     "\\omega_{{f_M}_1}",
-     "\\omega_{\\delta_S}",
-     "\\omega_\\theta",
-     "\\sigma_{AB}",
-     "\\sigma_{G2}"
-    )
-  }else{
-    tex2_names = c(
-     "{f_M}_1",
-     "\\delta_S",
-     "\\theta",
-     "\\delta_{Ab}",
-     "\\alpha_1",
-     "\\alpha_2",
-     "\\omega_{{f_M}_1}",
-     "\\omega_{\\delta_S}",
-     "\\omega_\\theta",
-     "\\sigma_{AB}",
-     "\\sigma_{G1}",
-     "\\sigma_{G2}"
-    )
-  }
-  if(type=="fixed_delta"){
-    tex2_names <- setdiff(tex2_names,c("\\delta_{Ab}","\\delta_S","\\omega_{\\delta_S}"))
-  }else if(type=="default"){
-    tex2_names <- setdiff(tex2_names,c("\\omega_{\\delta_S}"))
-  }else if(type=="only_S"){
-
-    tex2_names <- setdiff(tex2_names,c("\\delta_{Ab}","\\omega_{\\delta_S}"))
-  }
-}else if(files=="2"){
-  tex2_names = c(
-   "{f_M}_2",
-   "\\delta_S",
-   "\\theta_1",
-   "\\theta_2",
-   "\\alpha_1",
-   "\\omega_{{f_M}_1}",
-   "\\omega_{{f_M}_2}",
-   "\\omega_{\\theta_1}",
-   "\\omega_{\\theta_2}",
-   "\\sigma_{AB}",
-   "\\sigma_{G1}"
-  )
-}else if(files=="Mult"){
-  if(data=="Ab"){
-    if(type=="default"){
-      tex2_names = c(
-       "{f_M}_1",
-       "\\delta_S",
-       "\\theta",
-       "\\delta_{Ab}",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}"
-      )
-    }else if(type=="poor"){
-      tex2_names = c(
-       "\\delta_S",
-       "\\theta",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}"
-      )
-    }else if(type=="only_S"){
-      tex2_names = c(
-       "{f_M}_1",
-       "\\delta_S",
-       "\\theta",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}"
-      )
-    }
-  }else if(data=="Ab_G1_G3"){
-    if(type=="default"){
-      tex2_names = c(
-       "{f_M}_1",
-       "\\delta_S",
-       "\\theta",
-       "\\delta_{Ab}",
-       "\\alpha_1",
-       "\\alpha_3",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}",
-       "\\sigma_{G1}",
-       "\\sigma_{G3}"
-      )
-    }else if(type=="only_S"){
-      tex2_names = c(
-       "{f_M}_1",
-       "\\delta_S",
-       "\\theta",
-       "\\alpha_1",
-       "\\alpha_3",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}",
-       "\\sigma_{G1}",
-       "\\sigma_{G3}"
-      )
-    }
-  }else if(data=="Ab_G1"){
-    if(type=="default"){
-      tex2_names = c(
-       "{f_M}_1",
-       "\\delta_S",
-       "\\theta",
-       "\\delta_{Ab}",
-       "\\alpha_1",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}",
-       "\\sigma_{G1}"
-      )
-    }else if(type=="only_S"){
-      tex2_names = c(
-       "{f_M}_1",
-       "\\delta_S",
-       "\\theta",
-       "\\alpha_1",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}",
-       "\\sigma_{G1}"
-      )
-    }
-  }else if(data=="Ab_G3"){
-    if(type=="default"){
-      tex2_names = c(
-       "{f_M}_1",
-       "\\delta_S",
-       "\\theta",
-       "\\delta_{Ab}",
-       "\\alpha_3",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}",
-       "\\sigma_{G3}"
-      )
-    }else if(type=="only_S"){
-      tex2_names = c(
-       "{f_M}_1",
-       "\\delta_S",
-       "\\theta",
-       "\\alpha_3",
-       "\\omega_{{f_M}_1}",
-       "\\omega_\\theta",
-       "\\sigma_{AB}",
-       "\\sigma_{G3}"
-      )
-    }
-  }else if(type=="default"){
-    tex2_names = c(
-     "{f_M}_1",
-     "\\delta_S",
-     "\\theta",
-     "\\delta_{Ab}",
-     "\\alpha_1",
-     "\\alpha_2",
-     "\\alpha_3",
-     "\\alpha_4",
-     "\\alpha_5",
-     "\\omega_{{f_M}_1}",
-     "\\omega_\\theta",
-     "\\sigma_{AB}",
-     "\\sigma_{G1}",
-     "\\sigma_{G2}",
-     "\\sigma_{G3}",
-     "\\sigma_{G4}",
-     "\\sigma_{G5}"
-    )
-  }else if(type=="only_S"){
-    tex2_names = c(
-     "{f_M}_1",
-     "\\delta_S",
-     "\\theta",
-     "\\alpha_1",
-     "\\alpha_2",
-     "\\alpha_3",
-     "\\alpha_4",
-     "\\alpha_5",
-     "\\omega_{{f_M}_1}",
-     "\\omega_\\theta",
-     "\\sigma_{AB}",
-     "\\sigma_{G1}",
-     "\\sigma_{G2}",
-     "\\sigma_{G3}",
-     "\\sigma_{G4}",
-     "\\sigma_{G5}"
-    )
-  }
-
-}else if(files=="Mu"){
-  if(type=="only_S"){
-    tex2_names = c(
-     "{f_M}_1",
-     "\\delta_S",
-     "\\theta",
-     "\\mu_1",
-     "\\mu_2",
-     "\\mu_3",
-     "\\mu_4",
-     "\\mu_5",
-     "\\alpha_1",
-     "\\alpha_2",
-     "\\alpha_3",
-     "\\alpha_4",
-     "\\alpha_5",
-     "\\omega_{{f_M}_1}",
-     "\\omega_\\theta",
-     "\\sigma_{AB}",
-     "\\sigma_{G1}",
-     "\\sigma_{G2}",
-     "\\sigma_{G3}",
-     "\\sigma_{G4}",
-     "\\sigma_{G5}"
-    )
-  }
-}else if(files=="Mu_i"){
-  if(type=="only_S"){
-    tex2_names = c(
-     "{f_M}_1",
-     "\\delta_S",
-     "\\theta",
-     "\\mu_1",
-     "\\mu_2",
-     "\\mu_3",
-     "\\mu_4",
-     "\\mu_5",
-     "\\alpha_1",
-     "\\alpha_2",
-     "\\alpha_3",
-     "\\alpha_4",
-     "\\alpha_5",
-     "\\omega_{{f_M}_1}",
-     "\\omega_\\theta",
-     "\\omega_{mu_1}",
-     "\\omega_{mu_2}",
-     "\\omega_{mu_3}",
-     "\\omega_{mu_4}",
-     "\\omega_{mu_5}",
-     "\\sigma_{AB}",
-     "\\sigma_{G1}",
-     "\\sigma_{G2}",
-     "\\sigma_{G3}",
-     "\\sigma_{G4}",
-     "\\sigma_{G5}"
-    )
-  }
-}
+tex2_names = unname(c(fM1_pop = "{f_M}_1",
+                      delta_S_pop = "\\delta_S",
+                      theta_pop = "\\theta",
+                      delta_Ab_pop = "\\delta_{Ab}",
+                      alpha_1_pop = "\\alpha_1",
+                      alpha_2_pop = "\\alpha_2",
+                      omega_fM1 = "\\omega_{{f_M}_1}",
+                      omega_delta_S = "\\omega_{\\delta_S}",
+                      omega_theta = "\\omega_\\theta",
+                      sigma_AB = "\\sigma_{Ab}",
+                      sigma_G1 = "\\sigma_{G1}",
+                      sigma_G2 = "\\sigma_{G2}")[names(PopulationParameters)])
 options(scipen=999)
 
 stats_estim[,!(colnames(stats_estim) %in% c("Target Value","(NA)"))] <- apply(stats_estim[,!(colnames(stats_estim) %in% c("Target Value","(NA)"))],2, FUN=function(x){as.character(round(x,digits=precision+1))})
 
 
-cat("\n \n ----------- LATEX CODE FOR RESULTS -----------\n \n")
+# cat("\n \n ----------- LATEX CODE FOR RESULTS -----------\n \n")
 stats_estimLTX <- huxtable::huxtable(cbind(Parameter = paste0("$",tex2_names,"$"),stats_estim),add_rownames = F)[-1,]
 stats_estimLTX <- huxtable::set_escape_contents(stats_estimLTX,FALSE)
-print(xtable::xtable(stats_estimLTX,caption = paste0("Results of estimation, with bootstrap, for Irene model with",if(data=="Ab_G1-5"){" 5 genes"}else if(data=="Ab"){" anly antibodies measured"}else if(data=="Ab_G1" | data=="Ab_G3" ){" 1 gene"}else if(data=="Ab_G1_G3"){" 2 genes"}," using ",nBoot," bootstraps and ",nrep," replicates.\n")),sanitize.text.function=function(x){x},include.rownames = F)
+texTable = xtable::xtable(stats_estimLTX,caption = paste0("Results of estimation, with bootstrap, for Irene model with antibody Ab, ",if("G1" %in% data){" and a high noised gene,"}else if("G2" %in% data){" and a low noised gene,"}," using ",nBoot," bootstraps and ",nrep," replicates, with ",Nind," individuals.\n"))
+# print(texTable,sanitize.text.function=function(x){x},include.rownames = F)
+latex_code <- capture.output(print(texTable,sanitize.text.function=function(x){x},include.rownames = F))[-c(1,2)]
+writeLines(latex_code, paste0("Results/Results",files,if(Nind!=50){Nind},"_",paste0(data,collapse="_"),"/bootstrap",type,".txt"))
+
+
 
 
 stats_estimLTX <- cbind(Parameters=tex2_names,stats_estim) %>% flextable() %>%
   bold(par="header") %>%
   align(part="header",align="center") %>%
   align(part="body",align="right") %>%
-  add_header_lines(paste0("Results of estimation, with bootstrap, for Irene model with",if(data=="Ab_G1-5"){" 5 genes"}else if(data=="Ab"){" anly antibodies measured"}else if(data=="Ab_G1" | data=="Ab_G3"){" 1 gene"}else if(data=="Ab_G1_G3"){" 2 genes"}," using ",nBoot," bootstraps and ",nrep," replicates.\n")) %>%
+  add_header_lines(paste0("Results of estimation, with bootstrap, for Irene model with antibody Ab, ",if("G1" %in% data){" and a high noised gene,"}else if("G2" %in% data){" and a low noised gene,"}," using ",nBoot," bootstraps and ",nrep," replicates.\n")) %>%
   color(i=1,part="header",color="indianred") %>%
   fontsize(i=1,part="header",size=16)  %>%
   width(width=1,unit="cm") %>%
@@ -433,7 +187,7 @@ stats_estimLTX <- cbind(Parameters=tex2_names,stats_estim) %>% flextable() %>%
   vline(j=1,part="header", border = fp_border_default( width = 1))
 
 
-  save_as_html(stats_estimLTX, path = paste0("Results/Results",files,"_",data,"/bootstrap",type,".html"),expand=20000)
-  webshot2::webshot(url=paste0("Results/Results",files,"_",data,"/bootstrap",type,".html"),file=paste0("Results/Results",files,"_",data,"/bootstrap",type,".png"),quiet=TRUE)
-  unlink(paste0("Results/Results",files,"_",data,"/bootstrap",type,".html"))
+  save_as_html(stats_estimLTX, path = paste0("Results/Results",files,if(Nind!=50){Nind},"_",paste0(data,collapse="_"),"/bootstrap",type,".html"),expand=20000)
+  webshot2::webshot(url=paste0("Results/Results",files,if(Nind!=50){Nind},"_",paste0(data,collapse="_"),"/bootstrap",type,".html"),file=paste0("Results/Results",files,if(Nind!=50){Nind},"_",paste0(data,collapse="_"),"/bootstrap",type,".png"),quiet=TRUE)
+  unlink(paste0("Results/Results",files,if(Nind!=50){Nind},"_",paste0(data,collapse="_"),"/bootstrap",type,".html"))
 
